8afa4338bf8ab9287478874b9299f52cea955d63
Nikita Tyrkin <tyrkinn@pm.me>
2025-06-07 15:59:55 +0300

    feat: add book model to schema

--- src/com/tyrkinn/bookshelf/schema.clj
+++ src/com/tyrkinn/bookshelf/schema.clj
@@ -14,7 +14,16 @@
          [:xt/id       :msg/id]
          [:msg/user    :user/id]
          [:msg/text    :string]
-         [:msg/sent-at inst?]]})
+         [:msg/sent-at inst?]]
+
+   :book/id :uuid
+   :book [:map
+          [:xt/id       :book/id]
+          [:book/title  :string]
+          [:book/author :string]
+          [:book/isbn   :string]
+          [:book/price  {:optional true} :integer]
+          [:book/link   {:optional true} :string]]})
 
 (def module
   {:schema schema})
